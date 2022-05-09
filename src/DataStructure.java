b218e4000286917c5520fb2c14a7cc43c26b75d0
javokhir <javokhir.buvanazarov@gmail.com>
2022-05-08 22:58:58 -0500

    fix: deleted unused file

--- src/DataStructure.java
+++
@@ -1,8 +0,0 @@
-import java.lang.reflect.Array;
-import java.util.*;
-
-public class DataStructure {
-    public static void main(String[] args) {
-        System.out.println("Javokhir Buvanazarov");
-    }
-}
